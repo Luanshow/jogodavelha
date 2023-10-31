0c21ca817e2536a372c9a457a6b50e625c34d310
Luanshow <117780146+Luanshow@users.noreply.github.com>
2023-10-30 21:31:00 -0300

    luan

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 
+enum Player {
+  None='',
+  x='X',
+  o='o',
+
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  cells:Player[]=new Array(9).fill(Player.None);
+  
+  cells:Player[]=new Array(9). fill (Player.None);
+  currentPlayer:Player=Player.X;
+  winner:Player|null=null;
+  gameOver: boolean=false
+  
+  makeMove(index:number):void{
+    if(!this.cells[index] && !this.gameOver){
+      
+    }
+  }
 
   constructor() {}
 
